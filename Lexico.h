643318d3d1c6c86f709200528018dbb963602f2a
luisro369 <00005513@uca.edu.sv>
2019-10-06 17:19:55 -0600

    Agregando Lexico.h con un vector que contiene ''tokens''

---
+++ Lexico.h
@@ -0,0 +1,8 @@
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+//declarando el vector con los tokens
+vector<string> TOKENS = {"CONTS","ENT","FLOT","CAR","CAD","BOOL","SI","SINO","IGUAL","MAYOR","PARA","MIENTRAS","HACER","LECT","ESCR","PROC","FUNC","DEV","DEF","DIM","ING","LEER","RES","MULT","DIV","MOD","EXP","CONCAT","LOGY","LOGO","NEG"};
+
--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
-//archivos necesarios
 
+
+#include <algorithm>
+//archivos necesarios
+#include "Lexico.h"
 #include "CodeReader.h"
 
 using namespace std;
 
 int main (void){
 
+  //generar el vector con los strings de cada palabra para ser analizados
   vector<string> algo;
   algo = CodeReader("code.txt");
-  //recorriendo vector del archivo .txt
+  //recorriendo vector del archivo .txt y viendo si son iguales
   for(int i= 0; i < algo.size() ; i++){
+    if(find(TOKENS.begin(), TOKENS.end(), algo[i]) != TOKENS.end()){
+      cout<<"este elemento pertenece ";
+    }//if
     cout<<algo[i]<<"\n";
   }//for
   
