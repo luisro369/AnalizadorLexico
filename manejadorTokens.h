ed4f875a070f1152b399cddf9dedf2a997ed0006
Grafel093 <graf.el93@gmail.com>
2019-10-06 22:54:36 -0600

    Validaciones a los tipos de variables

--- manejadorTokens.h
+++ manejadorTokens.h
@@ -36,6 +36,22 @@ bool checkAlpha(string str){
     return retVal;
 }
 
+bool checkNumeric(string str){
+    bool retVal;
+    for(int i = 0; i < str.size()-1; i++){
+        if(isdigit(str[i]) == 0){
+            //retorna true si encontro algo que no es numero
+            retVal = true;
+            break;
+        }
+        else{
+            //todo bien
+            retVal = false;
+        }
+    }
+    return retVal;
+}
+
 
 //funcion de validaciones
 void evaluarVariable(string variable){
@@ -53,6 +69,72 @@ void evaluarVariable(string variable){
     }
 }
 
+void evaluarAsignar(string variable){
+    //validacion del caracter de asignación
+    if(variable != ":="){
+        cout<<"Error en la validacion del identificador\n debe ser :=\n";
+        //mata el programa
+        exit(0);
+    }
+    
+}
+
+void evaluarValor(string variable,int token){
+    //validacion del caracter ;
+    switch(token){
+        case 1:
+                if(checkNumeric(variable)){
+                    if(variable.length()>1){
+                        if(checkNumeric(variable.substr(0,variable.length()-1))){
+                            //mata el programa
+                            cout<<"el ingreso debe ser entero y terminar en ;\n";
+                            exit(0);
+                        }else{
+                            if(variable.substr(variable.length()-1,variable.length()) != ";"){
+                                //mata el programa
+                                cout<<"solo se permite terminar una linea de codigo con el caracter -> ;\n";
+                                exit(0);
+                            }
+                        }
+                    }
+                }
+        break;
+        case 2:
+                if(variable.length()>11){
+                    //mata el programa
+                    cout<<"solo se permite ingresar flotantes con magnitud de 5 enteros y 5 decimales maximo;\n";
+                    exit(0);
+                }
+        break;
+        case 3:
+                if(variable.length() != 3 && variable.length() != 4){
+                    //mata el programa
+                    cout<<"el ingreso de un caracter debe ser entre comillas simple pudiendo incluir el caracter -> ;\n";
+                    exit(0);
+                }
+                if(!(variable.substr(0,1) == "\'") && (variable.substr(2,variable.length()) == "\'")){
+                    //mata el programa
+                    cout<<"el ingreso de un caracter debe ser entre comillas simple pudiendo incluir el caracter ->;\n";
+                    exit(0);
+                }
+        break;
+        case 4:
+                if(variable.length()<2){
+                     //mata el programa
+                    cout<<"el ingreso de una cadena debe ser entre comillas dobles;\n";
+                    exit(0);
+                }    
+        break;
+        case 5:
+                if(variable != "VERDADERO" && variable != "FALSO" && variable != "VERDADERO;" && variable != "FALSO;"){
+                     //mata el programa
+                     cout<<"la variable solo acepta los valores VERDADERO Y FALSO;\n";
+                     exit(0);
+                }
+        break;
+    }
+}
+
 //funcion que manejara el flujo
 int bloque(vector<string> algo,int token,int posicion){
     //verifica si el token esta entre 1 y 5 porque son las de los tipo de datos
@@ -61,6 +143,10 @@ int bloque(vector<string> algo,int token,int posicion){
         posicion = posicion+1;
         //si es un identificador de tipo de datos por consecuencia sigue una variable asi que valida
         evaluarVariable(algo[posicion]);
+        posicion = posicion+1;
+        evaluarAsignar(algo[posicion]);
+        posicion = posicion+1;
+        evaluarValor(algo[posicion],token);
     }
     //se retorna la nueva posicion de lectura
     return posicion;
