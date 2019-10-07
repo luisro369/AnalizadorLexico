28bb32b87a9058ccc96e5eec832edab1c3130ea8
luisro369 <00005513@uca.edu.sv>
2019-10-07 14:20:49 -0600

    ya estan todas las funciones de los gemelos modularizadas

--- src/headers/manejadorTokens.h
+++ src/headers/manejadorTokens.h
@@ -5,6 +5,7 @@
 //archivos necesarios
 #include "EvaluarVariable.h"
 #include "EvaluarAsignacion.h"
+#include "EvaluarTipoValor.h"
 using namespace std;
 
 
@@ -18,86 +19,9 @@ int obtenerTokens(vector<string> algo,int posicion){
         }
     } 
     return 0;
-}
+}//obtener tokens
 
 
-
-bool checkNumeric(string str){
-    bool retVal;
-    for(int i = 0; i < str.size()-1; i++){
-        if(isdigit(str[i]) == 0){
-            //retorna true si encontro algo que no es numero
-            retVal = true;
-            break;
-        }
-        else{
-            //todo bien
-            retVal = false;
-        }
-    }
-    return retVal;
-}
-
-
-
-
-
-
-void evaluarValor(string variable,int token){
-    //validacion del caracter ;
-    switch(token){
-        case 1:
-                if(checkNumeric(variable)){
-                    if(variable.length()>1){
-                        if(checkNumeric(variable.substr(0,variable.length()-1))){
-                            //mata el programa
-                            cout<<"el ingreso debe ser entero y terminar en ;\n";
-                            exit(0);
-                        }else{
-                            if(variable.substr(variable.length()-1,variable.length()) != ";"){
-                                //mata el programa
-                                cout<<"solo se permite terminar una linea de codigo con el caracter -> ;\n";
-                                exit(0);
-                            }
-                        }
-                    }
-                }
-        break;
-        case 2:
-                if(variable.length()>11){
-                    //mata el programa
-                    cout<<"solo se permite ingresar flotantes con magnitud de 5 enteros y 5 decimales maximo;\n";
-                    exit(0);
-                }
-        break;
-        case 3:
-                if(variable.length() != 3 && variable.length() != 4){
-                    //mata el programa
-                    cout<<"el ingreso de un caracter debe ser entre comillas simple pudiendo incluir el caracter -> ;\n";
-                    exit(0);
-                }
-                if(!(variable.substr(0,1) == "\'") && (variable.substr(2,variable.length()) == "\'")){
-                    //mata el programa
-                    cout<<"el ingreso de un caracter debe ser entre comillas simple pudiendo incluir el caracter ->;\n";
-                    exit(0);
-                }
-        break;
-        case 4:
-                if(variable.length()<2){
-                     //mata el programa
-                    cout<<"el ingreso de una cadena debe ser entre comillas dobles;\n";
-                    exit(0);
-                }    
-        break;
-        case 5:
-                if(variable != "VERDADERO" && variable != "FALSO" && variable != "VERDADERO;" && variable != "FALSO;"){
-                     //mata el programa
-                     cout<<"la variable solo acepta los valores VERDADERO Y FALSO;\n";
-                     exit(0);
-                }
-        break;
-    }
-}
 
 //funcion que manejara el flujo
 int bloque(vector<string> algo,int token,int posicion){
@@ -114,6 +38,6 @@ int bloque(vector<string> algo,int token,int posicion){
     }
     //se retorna la nueva posicion de lectura
     return posicion;
-}
+}//bloque
 
 
